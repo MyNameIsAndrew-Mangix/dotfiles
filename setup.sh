25e622a27dc3c7635aec238352916e01450d6c2c
Andrew Klinsic <andrewklinsic@gmail.com>
2024-06-09 15:18:47 -0400

    setup checks for errors

--- setup.sh
+++ setup.sh
@@ -13,14 +13,23 @@ git pull
 
 echo "Setting up dotfiles..."
 
+# Iterate over each configuration file/directory
 for file in "${CONFIG_FILES[@]}"; do
-	# remove existing config if it exists
-	if [ -e ~.config/$file ]; then
-		echo "Removing existing $file configuration..."
-		rm -rf ~/.config/$file
-	fi
-	# create sym link
-	echo "Creating symlink for $file..."
-	ln -s $DOTFILES_DIR/$file ~/.config$file
+    # Remove existing config if it exists
+    if [ -e ~/.config/$file ]; then
+        echo "Removing existing $file configuration..."
+        rm -rf ~/.config/$file
+    fi
+    
+    # Create a symbolic link
+    echo "Creating symlink for $file..."
+    ln -s $DOTFILES_DIR/$file ~/.config/$file
+    
+    # Check if symlink was successfully created
+    if [ -L ~/.config/$file ]; then
+        echo "Symlink for $file created successfully."
+    else
+        echo "Failed to create symlink for $file."
+    fi
 done
 echo "Dotfiles setup complete."
