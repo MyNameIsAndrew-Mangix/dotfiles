e53759a92f749ab9d38535851890f243ac6bced5
Andrew Klinsic <andrewklinsic@gmail.com>
2024-06-09 15:16:42 -0400

    setup.sh checks for updates

--- setup.sh
+++ setup.sh
@@ -4,14 +4,23 @@ DOTFILES_DIR=~/dotfiles
 
 # list of config files/directories to symlink
 CONFIG_FILES=(awesome pulse kitty)
-	for file in "${CONFIG_FILES[@]}"; do
-		# remove existing config if it exists
-		if [ -e ~.config/$file ]; then
-			echo "Removing existing $file configuration..."
-			rm -rf ~/.config/$file
-		fi
-		# create sym link
-		echo "Creating symlink for $file..."
-		ln -s $DOTFILES_DIR/$file ~/.config$file
-	done
-	echo "Dotfiles setup complete."
+
+echo "Checking for updates..."
+cd $DOTFILES_DIR
+
+#pull the latest changes from the remote repo
+git pull
+
+echo "Setting up dotfiles..."
+
+for file in "${CONFIG_FILES[@]}"; do
+	# remove existing config if it exists
+	if [ -e ~.config/$file ]; then
+		echo "Removing existing $file configuration..."
+		rm -rf ~/.config/$file
+	fi
+	# create sym link
+	echo "Creating symlink for $file..."
+	ln -s $DOTFILES_DIR/$file ~/.config$file
+done
+echo "Dotfiles setup complete."
