9808dae6dd36cf8c6e0eb95cbb429620279eeb9f
Andrew Klinsic <andrewklinsic@gmail.com>
2024-06-09 15:10:49 -0400

    +kitty, +pulse, ~awesome

--- configuration/init.lua
+++
@@ -1,6 +0,0 @@
-return {
-    keys = require("configuration.keys"),
-    apps = require("configuration.apps"),
-    configuration = require("configuration.config")
-  }
-  
--- setup_monitors.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-#fixes small wibar while using 2 HPV27i's
-xrandr --dpi 90
